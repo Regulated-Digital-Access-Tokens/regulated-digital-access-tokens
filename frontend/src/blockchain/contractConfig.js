dcd7d98c188515faf18a4d0c7696cae7c8b76971
Ayan Mahata <91912251+RickyTheDude@users.noreply.github.com>
2026-07-16 18:49:27 +0530

    feat: add DEPLOYMENT_BLOCK to contract configuration and restrict event queries to optimize performance

--- frontend/src/blockchain/contractConfig.js
+++ frontend/src/blockchain/contractConfig.js
@@ -2,6 +2,8 @@ import RegulatedTokenArtifact from "./RegulatedToken.json";
 
 export const CONTRACT_ADDRESS = "0x6B37969185920d59234b5Dcf5Bf7Fa25fdA794d3";
 
+export const DEPLOYMENT_BLOCK = 10782136;
+
 export const IS_MOCK_ABI = false;
 
 export const CONTRACT_ABI = RegulatedTokenArtifact.abi;
